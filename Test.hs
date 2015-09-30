915fcd79bdf44fa15d09a9495b5bec9b202e018b
Maria Kovalenko <marya.i.kovalenko@gmail.com>
2015-09-30 16:51:20 +0600

    Задача 2.3.7. - класс типа Printable

--- Test.hs
+++ Test.hs
@@ -177,3 +177,23 @@ doItYourself = f . g . h
 h = (max 42)
 g = (^3)
 f = (logBase 2)
+
+-- 2.3.7
+-- Реализуйте класс типов Printable, предоставляющий один метод toString - функцию одной переменной, 
+-- которая преобразует значение типа, являющегося представителем Printable, в строковое представление.
+-- Сделайте типы данных Bool и () представителями этого класса типов, обеспечив следующее поведение:
+-- GHCi> toString True
+-- "true"
+-- GHCi> toString False
+-- "false"
+-- GHCi> toString ()
+-- "unit type"
+class Printable a where
+	toString :: a -> [Char]
+
+instance Printable Bool where
+	toString True = "true"
+	toString False = "false"
+
+instance Printable () where
+	toString () = "unit type"
