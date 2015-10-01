aca89aebd37c68667061c699292364dfaff7d91c
Maria Kovalenko <marya.i.kovalenko@gmail.com>
2015-10-01 12:02:02 +0600

    Задача 2.4.7 - класс SafeEnum

--- Test.hs
+++ Test.hs
@@ -242,10 +242,6 @@ class (KnownToGork a, KnownToMork a) => KnownToGorkAndMork a where
 -- Имея функцию ip = show a ++ show b ++ show c ++ show d определите значения a, b, c, d так, чтобы добиться следующего поведения:
 -- GHCi> ip
 -- "127.224.120.12"
-{-class (Int x, Show x) => MyInt x where
-	show :: x -> [Char]
-	show x = "."-}
---a, b, c :: MyInt 
 data MyIp = MyIp Int
 instance Show MyIp where
   show (MyIp a) = show a ++ "."
@@ -254,3 +250,32 @@ b = MyIp 224
 c = MyIp 120
 d = 12
 ip = show a ++ show b ++ show c ++ show d
+
+-- 2.4.7
+-- Реализуйте класс типов
+-- class SafeEnum a where
+--  ssucc :: a -> a
+ -- spred :: a -> a
+-- обе функции которого ведут себя как succ и pred стандартного класса Enum, однако являются тотальными, 
+-- то есть не останавливаются с ошибкой на наибольшем и наименьшем значениях типа-перечисления соответственно, 
+-- а обеспечивают циклическое поведение. Ваш класс должен быть расширением ряда классов типов стандартной библиотеки, 
+-- так чтобы можно было написать реализацию по умолчанию его методов, позволяющую объявлять его представителей без необходимости писать какой бы то ни было код. 
+-- Например, для типа Bool должно быть достаточно написать строку
+-- instance SafeEnum Bool
+-- и получить возможность вызывать
+-- GHCi> ssucc False
+-- True
+-- GHCi> ssucc True
+-- False
+
+class (Ord t, Enum t, Bounded t) => SafeEnum t where
+  ssucc :: t -> t
+  ssucc x = if x < maxBound then succ x
+  			else minBound 
+
+  spred :: t -> t
+  spred x = if x > minBound then pred x
+  			else maxBound
+
+instance SafeEnum Bool where
+
